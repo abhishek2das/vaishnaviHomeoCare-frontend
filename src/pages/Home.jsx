cd9cfab9e4bb5e8f3472a563d6a3e242de528831
Vishal Savita <vishal.savita@incraftiv.com>
2026-06-20 10:24:12 +0530

    Fixed parsing error detected while google crawling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -156,14 +156,13 @@ export default function Home() {
                   "addressRegion": "Chhattisgarh",
                   "postalCode": "492001",
                   "addressCountry": "IN"
-                  },
-                  "openingHours": ["Mo-Thu 11:00-13:30", "Fri 11:00-13:00"],
-                  "geo": {
-                    "@type": "GeoCoordinates",
-                    "latitude": 21.292918468904922,
-                    "longitude": 81.71982356729511
-                  }
-                  }
+                },
+                "openingHours": ["Mo-Thu 11:00-13:30", "Fri 11:00-13:00"],
+                "geo": {
+                  "@type": "GeoCoordinates",
+                  "latitude": 21.292918468904922,
+                  "longitude": 81.71982356729511
+                }
               },
               {
                 "@type": "WebSite",
